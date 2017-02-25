bbc7f0f65eb3fb5c82d98f3bf85abac289ebf84d
ProjectProgramAMark <projectprogramamark@gmail.com>
2017-02-25 17:19:37 -0500

    initialized pubnub

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+require('dotenv').config();
+var PubNub = require('pubnub');
 
 
 var mongoose = require('mongoose');
@@ -73,5 +74,45 @@ app.listen(6001, function() {
   console.log("Listening on 6001 ayyyyy lmao...");
 });
 
+
+// Initializing PubNub
+var pubnub = new PubNub({
+    subscribeKey: process.env.PUBNUB_SUBSCRIBE_KEY,
+    publishKey: process.env.PUBNUB_PUBLISH_KEY,
+    secretKey: process.env.PUBNUB_SECRET_KEY,
+    ssl: true
+});
+
+pubnub.subscribe({
+    channels: ['my_channel'],
+    withPresence: true // also subscribe to presence instances.
+});
+
+pubnub.addListener({
+
+    message: function(m) {
+        // handle message
+        var channelName = m.channel; // The channel for which the message belongs
+        var channelGroup = m.subscription; // The channel group or wildcard subscription match (if exists)
+        var pubTT = m.timetoken; // Publish timetoken
+        var msg = m.message; // The Payload
+    },
+    presence: function(p) {
+        // handle presence
+        var action = p.action; // Can be join, leave, state-change or timeout
+        var channelName = p.channel; // The channel for which the message belongs
+        var occupancy = p.occupancy; // No. of users connected with the channel
+        var state = p.state; // User State
+        var channelGroup = p.subscription; //  The channel group or wildcard subscription match (if exists)
+        var publishTime = p.timestamp; // Publish timetoken
+        var timetoken = p.timetoken;  // Current timetoken
+        var uuid = p.uuid; // UUIDs of users who are connected with the channel
+
+        console.log(p);
+    },
+    status: function(s) {
+        // handle status
+    }
+})
 
 module.exports = app;
